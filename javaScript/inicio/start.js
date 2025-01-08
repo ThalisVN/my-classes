fd8af2ace92c4a23e35263e38d516c43c82d3e0d
ThalisVN <thalisvn2003@gmail.com>
2025-01-08 15:02:14 -0300

    organizaion

--- javaScript/inicio/start.js
+++ javaScript/inicio/start.js
@@ -1,5 +1,5 @@
 /*comentarios ficam em verde é começado e "//", pode usar na linha inteira ou na frente do codigo,
- pode dessavar codigos de colocar na frente */
+ pode destravar codigos de colocar na frente */
 console.log("Bom dia") 
 
 {console.log("boa tarde") 
